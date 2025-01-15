ca9825632696d03d6a2223290c9d08df6a3350b0
Edoardo <scorzaedoardo@gmail.com>
2025-01-15 17:38:08 +0100

    gitignore added, webserver instruction added, progress on the webserver

--- webserver/app.py
+++ webserver/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 Scss(app)
 
 @app.route("/")
@@ -12,3 +14,21 @@ def index():
 
 if __name__ in "__main__":
     app.run(debug=True)
+
+@socketio.on("connect")
+def on_connect():
+    print("Client connected")
+    emit("server_message", {"message": "Welcome to the server!"})
+
+# Handle messages from the client
+@socketio.on("client_message")
+def handle_client_message(data):
+    print(f"Received from client: {data['message']}")
+    # Respond back to the same client
+    emit("server_message", {"message": f"Server received: {data['message']}"})
+
+# Server-side initiated messages
+def background_updates():
+    while True:
+        socketio.emit("server_message", {"message": "Periodic update from server"})
+        time.sleep(5)
