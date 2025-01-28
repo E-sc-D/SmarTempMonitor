cc67df0d7061ec1792110dba9320a7dabc323b89
Edoardo <scorzaedoardo@gmail.com>
2025-01-28 22:41:56 +0100

    small changes

--- webserver/app.py
+++ webserver/app.py
@@ -21,13 +21,13 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print(f"Message received: {msg.topic} -> {msg.payload.decode()}")
 
-broker = "192.168.1.5"
+""" broker = "192.168.1.5"
 port = 1883
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 client.connect(broker, port, 60)
-client.loop_forever()
+client.loop_forever() """
 
 
 @app.route("/")
