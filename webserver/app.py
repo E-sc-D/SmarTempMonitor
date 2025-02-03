3deaa68d7c49c91bfeff29d1a48b52cf409f2d23
Edoardo <scorzaedoardo@gmail.com>
2025-02-03 11:55:15 +0100

    code refactring and reordering

--- webserver/app.py
+++ webserver/app.py
@@ -3,17 +3,17 @@ import eventlet
 import random
 import serial
 
-eventlet.monkey_patch()
+eventlet.monkey_patch() #serve davvero????????
 
-from stopwatch import Stopwatch
-from temperature_fsm import TemperatureFSM  
+from utils.stopwatch import Stopwatch
+from utils.temperature_fsm import TemperatureFSM  
 from flask import Flask, render_template
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
 import paho.mqtt.client as mqtt
 
-windowState = False
+windowIsAuto = False
 timer = Stopwatch()
 fsm = TemperatureFSM(T1=30, T2=50, F1=2000, F2=500, DT=4)
 app = Flask(__name__)
@@ -27,13 +27,16 @@ def arduino_send(valore):
         arduino.write(f"{valore}\n".encode())  # Send data with newline
         print(f"Valore inviato: {valore}")
 
+#when is connected via mqtt to esp
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
     # Subscribe to a topic
     client.subscribe("esp32/temperature")
 
+#when receiving a message by esp32, mqtt
 def on_message(client, userdata, msg):
-    global windowState
+    global windowIsAuto,timer
+
     fsm.update(float(msg.payload.decode()), timer.resetElapsed())
     print(f"Message received: {msg.topic} -> {msg.payload.decode()}")
     socketio.emit("temp_reading", {"temp": msg.payload.decode(), 
@@ -41,7 +44,7 @@ def on_message(client, userdata, msg):
         "status" : fsm.get_state()})
     client.publish("CU/frequency", str(fsm.get_frequency()))
     arduino_send(f'temp:{msg.payload.decode()}')
-    if windowState:
+    if windowIsAuto:
         arduino_send(f'window:{fsm.window_percentage}')
 
 @app.route("/")
@@ -49,26 +52,27 @@ def index():
    return render_template("index.html")
 
 @socketio.on("connect") 
-def on_connect_web():
-    global arduino
+def on_webClient_connect():
+    global arduino,timer
+    
     print("Client connected")
     timer.start()
     arduino = serial.Serial(port='COM6', baudrate=9600, timeout=1)
 
 @socketio.on("client_data")
-def client_data(data):
+def on_webClient_data(data):
     arduino_send(f'window:{data["window"]}')
 
 @socketio.on("reset")
-def client_reset(data):
+def on_webClient_reset(data):
     fsm.resetState()
 
 @socketio.on("auto")
-def toggle_auto(data):
-    global windowState
-    windowState = not windowState
+def on_webClient_windowModeToggle(data):
+    global windowIsAuto
+    windowIsAuto = not windowIsAuto
 
-def background_task():
+def background_task(): #Questo potrebbe non essere necessario usando client_start
     broker = "192.168.1.5"
     port = 1883
     client = mqtt.Client()
